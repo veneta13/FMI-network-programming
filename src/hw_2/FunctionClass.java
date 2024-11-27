2deb4e8c09cef8c81527b1755dc42f62fd6c9bc1
veneta13 <venkireva80@gmail.com>
2024-11-27 11:30:27 +0200

    Create separate functions

--- src/hw_2/FunctionClass.java
+++ src/hw_2/FunctionClass.java
@@ -3,17 +3,25 @@ package hw_2;
 import java.util.*;
 
 public class FunctionClass {
-    public static void main(String[] args) {
-        List<Student> studentClass = new LinkedList<Student>();
+    private static List<Student> studentClass;
 
+    public static void main(String[] args) {
+        studentClass = new LinkedList<Student>();
+        addStudents();
+        Collections.sort(studentClass);
+        printStudents();
+    }
+
+    private static void addStudents() {
         studentClass.add(new Student("FMI1", "Ivan Petkov", 4.56));
         studentClass.add(new Student("FMI2", "Yoana Ivanova", 5.67));
         studentClass.add(new Student("FMI3", "Petar Dimitrov", 5.23));
         studentClass.add(new Student("FMI4", "Kalina Stoilova", 5.34));
         studentClass.add(new Student("FMI5", "Yordan Georgiev", 5.67));
+        studentClass.add(new Student("FMI6", "Yana Marinova", 5.67));
+    }
 
-        Collections.sort(studentClass);
-
+    private static void printStudents() {
         for (Student student : studentClass) {
             System.out.println(student);
         }
